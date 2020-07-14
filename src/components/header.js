1b9425449c0ec0a71a123547c8dad78183b1ea56
YU HAO WONG <WONGYUHAO@GMAIL.COM>
2020-07-14 12:52:08 +0800

    added link to old site

--- src/components/header.js
+++ src/components/header.js
@@ -4,9 +4,9 @@ import {Link} from 'gatsby'
 export default function Header(){
   return (
     <div>
-      <h1> This is a header </h1>
-      <p> bla bla bla I'm part of the header</p>
-      <Link >tic tac toe</Link>
+      <h1> Yu Hao Wong - UW CSE 2024</h1>
+      <p> This site in under construction. Visit the old site <a target="_blank" href="https://wongyuhao.github.io">here</a></p>
+
     </div>
   )
 }
