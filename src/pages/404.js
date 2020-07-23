b476ab26133283608c3dc133bd149ae590e3837e
YU HAO WONG <WONGYUHAO@GMAIL.COM>
2020-07-23 16:24:44 +0800

    added custom 404

---
+++ src/pages/404.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import {Card} from 'react-bootstrap'
+import {Link} from 'gatsby'
+
+export default () =>(
+  
+    <Card>
+      
+      <Card.Body className = 'text-center'>
+      <Card.Title>
+        <strong>404</strong>
+      </Card.Title>
+        There's nothing here! <br/>
+        <Link to='/'>Return to the homepage</Link>
+        
+      </Card.Body>
+    </Card>
+
+
+)
