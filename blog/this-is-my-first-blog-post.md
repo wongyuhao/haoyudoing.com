87981190ddfcccc83950f4cc1c9d7460db2aa04a
YU HAO WONG <WONGYUHAO@GMAIL.COM>
2020-08-23 20:55:22 +0800

    removed netlify cms

--- blog/this-is-my-first-blog-post.md
+++
@@ -1,6 +0,0 @@
----
-path: firstblog
-date: 2020-08-23T12:39:57.179Z
-title: This is my first blog post
----
-Yep. That;s me. I bet you're wondering how I got here.
--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@
 module.exports = {
   /* Your site config here */
   plugins: [
-    `gatsby-plugin-netlify-cms`,
+
     `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
