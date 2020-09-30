b6a945002edfb57462b9f107cf1c362709e7d432
Yu Hao Wong <wongyuhao@gmail.com>
2020-09-30 21:56:32 +0800

    fixed whitespace

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -12,19 +12,19 @@ import AngleUp from '../images/angle-up-solid.svg'
 export  default function Layout(props){
   return (
    
-      <SimpleReactLightbox>
-        <ScrollToTop smooth component={<img src={AngleUp}/>} />
+    <SimpleReactLightbox>
+    <ScrollToTop smooth component={<img src={AngleUp}/>} />
       <Helmet>
-          <meta charSet="utf-8" />
-          <title>Haoyudoing?</title>
-        </Helmet>
+        <meta charSet="utf-8" />
+        <title>Haoyudoing?</title>
+      </Helmet>
       <div>
         <Header />
         <Container className='mt-sm-2 mt-4'>
         {props.children}
         </Container>
       </div>
-    <Footer />
+        <Footer />
     </SimpleReactLightbox>
   
   )
